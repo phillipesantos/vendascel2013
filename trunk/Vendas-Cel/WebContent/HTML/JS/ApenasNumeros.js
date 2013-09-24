2c0dd6601bdf6467ef7793a558225038f4c5b415
andrephillipe.1993@gmail.com <andrephillipe.1993@gmail.com@9d89d527-5f09-b115-0cd5-b39daa610bdc>
2013-09-24 04:42:35 +0000

    att

---
+++ trunk/Vendas-Cel/WebContent/HTML/JS/ApenasNumeros.js
@@ -0,0 +1,9 @@
+function somenteNumeros(e){ // permite apenas numeros
+										 var tecla=(window.event)?event.keyCode:e.which;
+										 
+										 if((tecla > 47 && tecla < 58)) return true;
+										 else{
+												 if (tecla != 8) return false;
+												 else return true;
+											 }
+									}
